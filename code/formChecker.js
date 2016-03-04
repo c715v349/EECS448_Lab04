d598f094a3f9d5574370ed8f108795b8d803dbd6
Cammy Vo <c715v349@ku.edu>
2016-03-04 14:57:10 -0600

    Remove unused variables Remove some commentary

--- code/formChecker.js
+++ code/formChecker.js
@@ -5,11 +5,7 @@ function validation()
 	var human = parseInt(document.getElementsByName("human")[0].value);
 	var dino = parseInt(document.getElementsByName("dino")[0].value);
 	var feces = parseInt(document.getElementsByName("feces")[0].value);
-	var s0 = document.getElementsByName("shipping")[0].value;
-	var s1 = document.getElementsByName("shipping")[1].value;
-	var s2 = document.getElementsByName("shipping")[2].value;
 	
-	//this is used from a source
 	var emailFormat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	//^see http://www.regular-expressions.info/tutorial.html //^see http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
 	
